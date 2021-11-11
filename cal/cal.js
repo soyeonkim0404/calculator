ab01d1613bc30a42777802251d93fb9b0d0b37ac
김소연_이모션 <soyeon.kim1@emotion.co.kr>
2021-11-11 13:39:16 +0900

    예외처리 공부

--- cal/cal.js
+++ cal/cal.js
@@ -2,14 +2,27 @@ class Calculator{
     constructor(displayElement){
         this.displayElement = displayElement;
         this.displayContent = '';
+        this.operatorChecked = true; //숫자 전에 연산자 입력 방지
+        this.equalsChecked = false;
     }
 
     appendNumber(number){
-        this.displayContent = this.displayContent + number; 
+        if(this.equalsChecked){
+            this.displayContent =  number; 
+            this.equalsChecked = false;
+        }else{
+            this.displayContent +=  number; 
+        }
+         this.operatorChecked = false; // 숫자 입력 후 연산자 입력 가능하게 false 처리
     }
 
     appendOperator(operator){
-        this.displayContent = this.displayContent + operator;
+        if(this.operatorChecked) return false;//연산자가 있을 경우 더블 입력 방지
+        if(this.equalsChecked){
+            this.equalsChecked = false;
+        };
+        this.displayContent += operator;
+        return this.operatorChecked = true; //연산자 + 연산자 입력 방지
     }
 
     updateDisplay(){
@@ -19,10 +32,13 @@ class Calculator{
     clear(){
         this.displayElement.value = 0;
         this.displayContent = '';
+        this.operatorChecked = true;
     }
 
     compute(){
+        if(this.operatorChecked) return;
         this.displayContent = eval(this.displayContent);
+        this.equalsChecked = true;
     }
 }
 
